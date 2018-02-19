ff8c1dea3070f5b47cf5278c95dd7ddfa9310f94
Ekaterina Pavlova <pavlova.ea.91@gmail.com>
2018-02-20 01:12:15 +0300

    Implemented function, which converts Celsius to Fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function warmup(temperature) {
+module.exports = function warmup(tempCelsius) {
   // your implementation here
+    var tempFahrenheit = 9/5 * tempCelsius + 32;
+    return tempFahrenheit;
 };
